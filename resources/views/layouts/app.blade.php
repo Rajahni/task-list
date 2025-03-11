837788b966dc1fe6d991750181028d0882839846
Rajahni <crajahni@yahoo.com>
2025-03-11 00:57:23 -0500

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,11 +2,17 @@
 <html>
     <head>
         <title>Laravel 12 Task List App</title>
+        @yield('styles')
     </head>
 
     <body>
         <h1>@yield('title')</h1>
         <div>
+            @if (session()->has('success'))
+            <div>
+                {{ session('success') }}
+            </div>
+            @endif
             @yield('content')
         </div>
     </body>
